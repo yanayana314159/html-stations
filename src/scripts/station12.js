706ce1e145d5d0340862115eaf9a22fe56e7eab6
yanahirogit config --global user.email yanahiro0214@gmail.com <yanahiro0214@gmail.com>
2023-08-14 15:26:58 +0900

    Rank1 clear

--- src/scripts/station12.js
+++ src/scripts/station12.js
@@ -1,3 +1,7 @@
-function getData() {
-    return "Station12をクリア!"
-}
+//function getData() {
+//  return "Station12をクリア";
+//}
+
+var result = "Station12をクリア!";
+var elem = document.getElementById("result");
+elem.textContent = "Station12をクリア!";
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,32 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      const namedata = userList.map(test);
+      resolve(namedata);
+    }, 3000);
+  });
+  return await promise;
 }
 
-function test() {
-  return
+function test(data) {
+  data.full_name = data.family_name + " " + data.first_name;
+  return data;
 }
-
